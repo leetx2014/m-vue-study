414ff3227ba0a36f4a9e95954c2129c51aa3fe3b
leetx <1982492725@qq.com>
2020-08-04 20:50:49 +0800

    update: use vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    counter: 0
   },
   mutations: {
+    add(state) {
+      state.counter++
+    }
   },
   actions: {
+    add({ commit }) {
+      setTimeout(() => {
+        commit('add')
+      }, 1000);
+    }
   },
   modules: {
   }
